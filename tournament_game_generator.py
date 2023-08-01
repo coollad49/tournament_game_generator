becdcf17b4946966d390818e48ed5ba967eaa142
Lucas <daniellucasadebayo@gmail.com>
2023-08-01 22:36:55 +0100

    add new function that gets the num of games played

--- tournament_game_generator.py
+++ tournament_game_generator.py
@@ -25,16 +25,29 @@ def get_team_names(num_teams):
                     print("Team names must have at least 2 characters, try again.")
 
                 team_name = input(f"Enter the name for team #{count}: ")
-                if len(team_name.split()) <= 2 and len(team_name) > 2:
+                if len(team_name.split()) <= 2 and len(team_name) >= 2:
                     break
 
         team_names.append(team_name)
         if count == num_teams:
             break
+    return team_names
 
+def get_number_of_games_played(num_teams):
+    num_of_played_games = int(input("Enter The number of games Played by each team: "))
+    if num_of_played_games < num_teams -1:
+        print("Invalid number of games. Each team plays each other at least once in the regular season, try again.")
+        while True:
+            num_of_played_games = int(input("Enter The number of games Played by each team: "))
+            if num_of_played_games >= num_teams -1:
+                break
+            else:
+                print("Invalid number of games. Each team plays each other at least once in the regular season, try again.")
+    return num_of_played_games
 
 
 num_teams = get_number_of_teams()
 team_names = get_team_names(num_teams)
+games_played = get_number_of_games_played(num_teams)
 
 print("Generating the games to be played in the first round of the tournament...")
